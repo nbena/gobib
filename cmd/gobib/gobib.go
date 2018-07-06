9071d1aef132bcb080e23ba751999af31baccf3d
nbena <ciaobena@outlook.it>
2018-07-06 19:02:11 +0200

    first work on cmd

--- cmd/gobib/gobib.go
+++ cmd/gobib/gobib.go
@@ -25,12 +25,16 @@ func setFlags() {
 	flag.IntVar(&year, "default-year", 0, "the default year value to use when a year is not found")
 	flag.StringVar(&defaultVisited, "default-urldate", "", "the default urldate value to use, in a form YYYY-MM-DD")
 	flag.BoolVar(&printFinished, "print-finished", false, "print a message when conversion is finished")
+
+	flag.Parse()
 }
 
 func main() {
 
-	flag.Parse()
+	setFlags()
 	var err error
+
+	var finalDefaultVisited *time.Time
 
 	if defaultVisited != "" {
 		visited, err = time.Parse("2006-01-02", defaultVisited)
@@ -38,25 +42,44 @@ func main() {
 			fmt.Fprintf(os.Stderr, "Error in 'default-urldate' format")
 			os.Exit(-1)
 		}
+		finalDefaultVisited = &visited
+	} else {
+		finalDefaultVisited = nil
 	}
+
 	inputFile, err := os.Open(input)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error opening file %s: %s", input, err.Error())
 		os.Exit(-1)
 	}
-	outputFile, err := os.Open(input)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error opening file %s: %s", output, err.Error())
-		os.Exit(-1)
+	var outputFile *os.File
+	if output == "/dev/stout" || output == "" {
+		outputFile = os.Stdout
+	} else {
+		outputFile, err = os.Open(output)
+		if err != nil {
+			if os.IsNotExist(err) {
+				outputFile, err = os.Create(output)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Fail to create file: %s, %s", output, err.Error())
+					os.Exit(-1)
+				}
+			} else {
+				fmt.Fprintf(os.Stderr, "Error opening file %s: %s", output, err.Error())
+				os.Exit(-1)
+			}
+		}
 	}
-	input := bufio.NewReader(inputFile)
-	output := bufio.NewWriter(outputFile)
+
+	in := bufio.NewReader(inputFile)
+	out := bufio.NewWriter(outputFile)
+	// var out strings.Builder
 
 	config := &gobib.Config{
-		Input:          input,
-		Output:         output,
-		DefaultYear:    0,
-		DefaultVisited: &visited,
+		Input:          in,
+		Output:         out,
+		DefaultYear:    year,
+		DefaultVisited: finalDefaultVisited,
 	}
 
 	converter := gobib.NewConverter(config)
